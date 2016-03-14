88a5d75101b9c076e40573f0e90e50f4a6ecdd30
Gilles B <bonhoure.gilles@gmail.com>
2016-03-14 20:15:25 +0100

    Objectif 4

--- src/jus/aoo/tri/sort/MaximumSort.java
+++ src/jus/aoo/tri/sort/MaximumSort.java
@@ -4,7 +4,13 @@ import java.util.Comparator;
 
 import jus.aoo.tri.Tri;
 
+/**
+ * Coût théorique : O(somme de 1 à N)
+ *
+ */
 public class MaximumSort extends Tri {
+	
+	public int ite = 0;
 
 	@Override
 	protected <T> T[] sort(T[] t, Comparator<T> c) {
@@ -13,6 +19,11 @@ public class MaximumSort extends Tri {
 			max = rangmax(t,i,c);
 			swap(t,max,i);
 		}
+		int somme = 0;
+		for(int i = 1; i<=t.length; i++) {
+			somme += i;
+		}
+		System.out.println("Vérification, somme de 1 à "+ t.length + " ( " + somme + " ) ?= coût théorique : " + ite);
 		return t;
 	}
 	
@@ -20,8 +31,9 @@ public class MaximumSort extends Tri {
 		int max;
 		max = 0;
 		
-		for (int i = 0; i < taille; i++) {
+		for (int i = 0; i <= taille; i++) {
 			if(c.compare(t[i],t[max])>0)max = i;
+			ite++;
 		}
 		return max;
 		
